c36dbbd3d3681a728db92ca653a81fd09122933d
David Zbarsky <dzbarsky@gmail.com>
2026-04-26 22:57:06 -0400

    [prost] Upgrade default toolchains (#59)

--- test/prost/prost_smoke_test.rs
+++ test/prost/prost_smoke_test.rs
@@ -1,10 +1,15 @@
-use echo_proto::echo::EchoRequest;
+use echo_proto::echo::{
+    EchoRequest,
+    echo_service_client::EchoServiceClient,
+};
 
 #[test]
-fn generated_message_is_constructible() {
+fn generated_message_and_grpc_client_types_are_available() {
     let request = EchoRequest {
         message: "hello".to_owned(),
     };
+    let client_type = std::any::type_name::<EchoServiceClient<()>>();
 
     assert_eq!(request.message, "hello");
+    assert!(client_type.contains("EchoServiceClient"));
 }
