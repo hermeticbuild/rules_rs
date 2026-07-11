0f1f143dbe25c54dafd910087861050672436fdb
Daisuke Nishimatsu <daisuke@DaisukenoMacBook-Air.local>
2026-07-11 11:47:49 +0900

    address review for header
    Signed-off-by: Daisuke Nishimatsu <daisuke@DaisukenoMacBook-Air.local>

--- 3rd_party/coreaudio-sys/coreaudio.h
+++ 3rd_party/coreaudio-sys/coreaudio.h
@@ -1,6 +1,5 @@
 #include <AudioUnit/AudioUnit.h>
 #include <AudioToolbox/AudioToolbox.h>
-#include <CoreAudio/CoreAudio.h>
 #include <CoreMIDI/CoreMIDI.h>
 #include <OpenAL/al.h>
 #include <OpenAL/alc.h>
@@ -8,7 +7,12 @@
 #if defined(__APPLE__)
 #include <TargetConditionals.h>
 #if TARGET_OS_OSX
+#include <CoreAudio/CoreAudio.h>
 #include <CoreAudio/AudioServerPlugIn.h>
 #include <IOKit/audio/IOAudioTypes.h>
+#else
+#include <CoreAudio/CoreAudioTypes.h>
 #endif
+#else
+#include <CoreAudio/CoreAudio.h>
 #endif
